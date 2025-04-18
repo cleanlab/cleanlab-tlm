8f9025ecf137791229c0ad327a3d5406ca218ce3
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2025-04-18 13:17:26 -0400

    remove most beta models from testing -- too slow

--- tests/constants.py
+++ tests/constants.py
@@ -30,10 +30,12 @@ excluded_tlm_models: list[str] = [
     "o1-mini",
     "o3",
     "o3-mini",
+    "o4-mini",
     "nova-lite",
     "nova-pro",
     "gpt-4",
     "gpt-4.1",
+    "gpt-4.1-mini",
     "gpt-4.5-preview",
 ]
 VALID_TLM_MODELS: list[str] = [model for model in _VALID_TLM_MODELS if model not in excluded_tlm_models]
