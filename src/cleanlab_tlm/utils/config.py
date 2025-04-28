4e24abc8a0b8d40ae359ec584e4f07b073be3f0d
Ashish Sardana <ashishsardana21@gmail.com>
2025-04-28 10:53:01 -0700

    Init

---
+++ src/cleanlab_tlm/utils/config.py
@@ -0,0 +1,43 @@
+from cleanlab_tlm.internal.constants import (
+    _TLM_DEFAULT_MODEL,
+    _DEFAULT_TLM_QUALITY_PRESET,
+    _TLM_DEFAULT_CONTEXT_LIMIT,
+    _TLM_MAX_TOKEN_RANGE
+)
+
+def get_default_model() -> str:
+    """
+    Get the default model name for TLM.
+
+    Returns:
+        str: The default model name for TLM.
+    """
+    return _TLM_DEFAULT_MODEL
+
+def get_default_quality_preset() -> str:
+    """
+    Get the default quality preset for TLM.
+
+    Returns:
+        str: The default quality preset for TLM.
+    """
+    return _DEFAULT_TLM_QUALITY_PRESET
+
+def get_default_context_limit() -> int:
+    """
+    Get the default context limit for TLM.
+
+    Returns:
+        int: The default context limit for TLM.
+    """
+    return _TLM_DEFAULT_CONTEXT_LIMIT
+
+
+def get_default_max_tokens() -> int:
+    """
+    Get the default maximum output tokens allowed.
+
+    Returns:
+        int: The default maximum output tokens.
+    """
+    return _TLM_MAX_TOKEN_RANGE["default"][1]
