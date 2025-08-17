cfc6b825d9a17d22c6e9c5f6df6b2ad89d34c032
Hui Wen <45724323+huiwengoh@users.noreply.github.com>
2025-08-17 07:48:08 -0400

    remove gpt-5 models from beta (#110)

--- src/cleanlab_tlm/tlm.py
+++ src/cleanlab_tlm/tlm.py
@@ -589,8 +589,7 @@ class TLMOptions(TypedDict):
          "o3-mini", "o1", "o1-mini", "gpt-4", "gpt-3.5-turbo-16k", "claude-opus-4-0", "claude-sonnet-4-0", "claude-3.7-sonnet",  \
          "claude-3.5-sonnet-v2", "claude-3.5-sonnet", "claude-3.5-haiku", "claude-3-haiku", "nova-micro", "nova-lite", "nova-pro"}, default = "gpt-4.1-mini"): \
         Underlying base LLM to use (better models yield better results, faster models yield faster results).
-        - Models still in beta: "gpt-5", "gpt-5-mini", "gpt-5-nano", "o3", "o1", "o4-mini", "o3-mini", "o1-mini", "gpt-4.5-preview", \
-            "claude-opus-4-0", "claude-sonnet-4-0", "claude-3.7-sonnet", "claude-3.5-haiku".
+        - Models still in beta: "o3", "o1", "o4-mini", "o3-mini", "o1-mini", "gpt-4.5-preview", "claude-opus-4-0", "claude-sonnet-4-0", "claude-3.7-sonnet", "claude-3.5-haiku".
         - Recommended models for accuracy: "gpt-5", "gpt-4.1", "o4-mini", "o3", "claude-opus-4-0", "claude-sonnet-4-0".
         - Recommended models for low latency/costs: "gpt-4.1-nano", "nova-micro".
 
