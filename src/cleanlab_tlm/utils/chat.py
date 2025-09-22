f4a2ebcd2cf749947ec405fe18104522168aa521
Gordon Lim <gordonlim@Gordons-MBP.attlocal.net>
2025-09-22 16:13:53 -0700

    update

--- src/cleanlab_tlm/utils/chat.py
+++ src/cleanlab_tlm/utils/chat.py
@@ -64,7 +64,7 @@ _TOOL_CALL_SCHEMA_PREFIX = (
     f"{_TOOL_CALL_TAG_START}\n"
     "{'name': <function-name>, 'arguments': <args-dict>}\n"
     f"{_TOOL_CALL_TAG_END}\n\n"
-    "Function calls may optionally include a call_id, which should be ignored."
+    "Function calls and their results may optionally include a call_id, which should be ignored."
 )
 
 
